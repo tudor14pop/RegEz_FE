2fdd722a1b9eec75dae7dcc66a73127fdd7addc1
Norbert Simon <norbert.simon@servustech.com>
2020-06-18 01:29:01 +0300

    first commit

---
+++ RegEz_FE/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [
+{
+  path: '',
+  component: LoginComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
