d8299134e2af42054e696f53b08b6baf5b546cc6
Norbert Simon <norbert.simon@servustech.com>
2020-07-17 17:25:27 +0300

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 const IP_ADDRESS_REGEX = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
-const webProtocol = IP_ADDRESS_REGEX.test(window.location.host) ? 'http' : 'http';
+const webProtocol = IP_ADDRESS_REGEX.test(window.location.host) ? 'http' : 'https';
 
 export const environment = {
   production: true,
